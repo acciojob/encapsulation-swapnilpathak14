696d8e19067013bbe26131f8d7c46a82253a0971
Swapnil Pathak <61601014+swapnilpathak14@users.noreply.github.com>
2023-01-30 12:23:43 +0530

    Recent Changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,13 @@ package com.driver;
 
 public class Main {
   
+  public static void main(String[] args) {
+        RWOnly obj=new RWOnly();
+
+        //obj.name="Swapnil";  //error will throw
+
+        obj.setName("Swapnil");
+        obj.getName();
+    }
+  
 }
